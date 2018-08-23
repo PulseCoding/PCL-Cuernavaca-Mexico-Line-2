de22218dd1b50a0a90023fa4140750c9b1b21cd7
RobbKryev <34248369+RobbKryev@users.noreply.github.com>
2018-08-23 15:12:00 -0500

    Update unslave.js

--- unslave.js
+++ unslave.js
@@ -4,7 +4,7 @@ var Service = require('node-windows').Service
 var svc = new Service({
   name:'PULSE line2 SERVICE',
   description: 'Control of the PULSE code',
-  script: __dirname + '\\mex_cue_pcl_line2_wise.js',
+  script: __dirname + '\\mex_cue_pcl_line2_wise__old.js',
   env: {
     name: "HOME",
     value: process.env["USERPROFILE"]
