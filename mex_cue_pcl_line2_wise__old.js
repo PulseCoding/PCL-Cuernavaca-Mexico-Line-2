feff8f02834f22a7f8ce8acf38f21a7ecbf0a66b
RobbKryev <34248369+RobbKryev@users.noreply.github.com>
2018-08-24 10:45:11 -0500

    Update mex_cue_pcl_line2_wise__old.js

--- mex_cue_pcl_line2_wise__old.js
+++ mex_cue_pcl_line2_wise__old.js
@@ -489,6 +489,8 @@ client1.on('connect', function(err) {
                 }
               }
 		
+                    fs.writeFileSync('Var.json','{"Date": ' + Date.now() + '",Index": ' + IndexFillerReject +'}')
+		
 		if(CntInFiller - CntOutFiller - FillerReject.rejected != 0 && IndexFillerReject==300){
                     FillerdeltaRejected = CntInFiller - CntOutFiller - FillerReject.rejected
                     FillerReject.rejected = CntInFiller - CntOutFiller
