cbee805246e7cf0cdde0f3f34f8b077d85d9d8f9
RobbKryev <34248369+RobbKryev@users.noreply.github.com>
2018-08-24 11:39:51 -0500

    Update mex_cue_pcl_line2_wise__old.js

--- mex_cue_pcl_line2_wise__old.js
+++ mex_cue_pcl_line2_wise__old.js
@@ -492,7 +492,7 @@ client1.on('connect', function(err) {
 		console.log(typeof(FillerReject.rejected))
 		console.log(FillerReject)
 		
-		console.log('In: '+CntInFiller+ 'Out: '+ CntOutFiller + ' Rej: ' + FillerReject.rejected +' Con1: '+ CntInFiller - CntOutFiller - FillerReject.rejected + ',Con2: '+ IndexFillerReject)               
+		console.log('In: '+CntInFiller+ 'Out: '+ CntOutFiller + ' Rej: ' + FillerReject.rejected +' Con1: '+ (CntInFiller - CntOutFiller - FillerReject.rejected) + ',Con2: '+ IndexFillerReject)               
 		if(CntInFiller - CntOutFiller - FillerReject.rejected != 0 && IndexFillerReject==300){
                     FillerdeltaRejected = CntInFiller - CntOutFiller - FillerReject.rejected
                     FillerReject.rejected = CntInFiller - CntOutFiller
